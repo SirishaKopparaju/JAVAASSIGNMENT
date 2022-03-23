affdc8674bdce7c3232e475da07b614fbe16c0c1
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 19:59:08 +0530

    added quotient and reminder problem

--- src/Day4/BasicCoreJava.java
+++ src/Day4/BasicCoreJava.java
@@ -3,16 +3,16 @@ package Day4;
 public class BasicCoreJava {
 	public static void main(String[]args) {
 
-	double FLIP=Math.floor(Math.random()*10)%2;
-	if(FLIP==1) {
-		System.out.println("coin is heads");
-	}
-	else {
-		System.out.println("coin is tails");
+	int dividend=50,divisor=5;
+	int quo=dividend/divisor;
+	int rem=dividend%divisor;
+	System.out.println("quotient:"+quo);
+	System.out.println("reminder:"+rem);
 
+	
 		
 	}
 }
-}
+
 
 
