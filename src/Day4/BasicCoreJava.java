9f4286d8a6751a8569d6bd3c84ee30224166cd85
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 19:47:52 +0530

    added power program

--- src/Day4/BasicCoreJava.java
+++ src/Day4/BasicCoreJava.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class BasicCoreJava {
 	public static void main(String[]args) {
 		Scanner sc=new Scanner(System.in);
-		int n;
+		int n,p,res=1;
 		double sum=0.0;
 		System.out.println("enter n value");
 		n=sc.nextInt();
-		while(n>0)
-		{
-			sum+=(1.0)/n;
-			n--;
-			
+		System.out.println("enter p value");
+
+		p=sc.nextInt();
+
+		for (int i=1;i<=p;i++) {
+			res=n*res;
 		}
-		System.out.println("harmonic sum is"+sum);
+		System.out.println("power is"+res);
 	}
 }
 
