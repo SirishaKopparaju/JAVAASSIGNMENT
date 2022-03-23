d6dbae3bd0d847b6f83161be730d19d96989e28f
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 19:24:36 +0530

    ADDED SWAPING PROGRAM

---
+++ src/Day4/BasicCoreJava.java
@@ -0,0 +1,13 @@
+package Day4;
+
+public class BasicCoreJava {
+	public static void main(String[]args) {
+		int a=10,b=20;
+		System.out.println("Before Swaping:"+a +b);
+		int t=a;
+		a=b;
+		b=t;
+	
+	System.out.println("after Swaping:"+a +b);
+	}
+}
