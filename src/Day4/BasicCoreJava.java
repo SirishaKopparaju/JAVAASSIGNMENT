5a6d2594d7ee2a080d7f7d60825c1fb1e2f29441
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-23 19:37:15 +0530

    added factorial program

--- src/Day4/BasicCoreJava.java
+++ src/Day4/BasicCoreJava.java
@@ -1,19 +1,16 @@
 package Day4;
 
-import java.util.Scanner;
 
 public class BasicCoreJava {
 	public static void main(String[]args) {
-		Scanner sc =new Scanner(System.in);
-		System.out.println("enter the number");
-
-		int num=sc.nextInt();
-		if(num%2==0) {
-			System.out.println("its even number");
+		int num=10;
+		long factorial=1;
+		for(int i=1;i<num;i++)
+		{
+			factorial=factorial*i;
+			
 		}
-			else {
-				System.out.println("its odd number");
-			}
-			}
+		System.out.println("factorial of number is" +factorial);
 		}
+}
 
