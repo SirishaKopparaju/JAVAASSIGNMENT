ce55f35a43e4ce1d104b45e4733306dda8f89a6a
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-25 17:13:37 +0530

    REFACTOR:2D Array program

--- src/Day4/BasicCoreJava.java
+++ src/Day4/BasicCoreJava.java
@@ -1,18 +1,43 @@
 package Day4;
 
+import java.util.Scanner;
+
 public class BasicCoreJava {
 	public static void main(String[]args) {
-
-	int dividend=50,divisor=5;
-	int quo=dividend/divisor;
-	int rem=dividend%divisor;
-	System.out.println("quotient:"+quo);
-	System.out.println("reminder:"+rem);
-
-	
-		
+	 
+	                Scanner scan = new Scanner(System.in);
+	 
+	                int inputArr[][] = new int[5][5];
+	 
+	                System.out.println("Enter the number of rows : ");
+	                int noOfRows = scan.nextInt();
+	                System.out.println("Enter the number of columns : ");
+	                int noOfCols = scan.nextInt();
+	 
+	                int noOfElements = noOfRows * noOfCols;
+	 
+	                System.out.println("Please enter " + noOfElements + " elements nows.");
+	 
+	                // read array elements row wise.
+	                for (int i = 0; i < noOfRows; i++) {
+	                        for (int j = 0; j < noOfRows; j++) {
+	                                inputArr[i][j] = scan.nextInt();
+	                        }
+	                }
+	 
+	                // close the scanner
+	                scan.close();
+	                System.out.println("The Input array is :");
+	                for (int i = 0; i < noOfRows; i++) {
+	                        for (int j = 0; j < noOfRows; j++) {
+	                                System.out.print(inputArr[i][j] + "\t");
+	                        }
+	                        System.out.println();
+	                }
+	        }
 	}
-}
+		
+	
 
 
 
