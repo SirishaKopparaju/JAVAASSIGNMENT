3f8d672cfece69dada23d031f27b57d28cf59e37
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-26 22:31:37 +0530

    added notes program

--- src/Day6/LogicalPrograms.java
+++ src/Day6/LogicalPrograms.java
@@ -1,21 +1,39 @@
 package Day6;
-
-
-
-
 public class LogicalPrograms {
+	public static int main(String[] args) {
+	int i;
+	int total;
 	
-	public static void main(String[] args)   
-	{  
-	int number = 987654, reverse = 0;  
-	while(number != 0)   
-	{  
-	int remainder = number % 10;  
-	reverse = reverse * 10 + remainder;  
-	number = number/10;  
-	}  
-	System.out.println("The reverse of the given number is: " + reverse);  
-	}  
+   int[] notes = { 1000,500,100,50,10,5,2,1};
+   int money;
+  	
+  	
+        int rem;
+		if(money==0)
+		{
+			return -1 ;
+		}
+		else
+		{
+			if(money>=notes[i])
+			{
+				int calNotes =money/notes[i];
+				rem = money%notes[i];
+				money =rem;
+				total += calNotes;
+				System.out.println(notes[i]+   " Notes ---> " +calNotes );
+			}
+			i++;
+			return calculate(money, notes);
+		}
 	}
+
+	private static int calculate(int money, int[] notes) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
+}
 
 
