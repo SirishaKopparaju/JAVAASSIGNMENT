57248b26ab283c35bb02aeb188e500bb1f72aabf
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-26 22:04:23 +0530

    added wind mill program

--- src/Day4/BasicCoreJava.java
+++ src/Day4/BasicCoreJava.java
@@ -1,28 +1,21 @@
 package Day4;
 
-public class BasicCoreJava {
-public static void main(String args[])
-	
-	{     	long x1,x2,y1,y2;
- 
-		double dis;
- 
-                x1=Long.parseLong(args[0]);
-		
-                y1=Long.parseLong(args[1]);
- 
-	        x2=Long.parseLong(args[2]);
-		
-                y2=Long.parseLong(args[3]); 
-		     
-	        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
- 	 	        
-  	        System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
- 
-	}
-		
 
-}
+
+public class BasicCoreJava {
+	public static void main(String[] args) {
+        double t = Double.parseDouble(args[0]);
+        double v = Double.parseDouble(args[1]);
+        double w = 35.74 + 0.6215*t + (0.4275*t - 35.75) * Math.pow(v, 0.16);
+        System.out.println("Temperature = " + t);
+        System.out.println("Wind speed  = " + v);
+        System.out.println("Wind chill  = " + w);
+    }
+
+	
+
+	}
+
 		 
 
 
