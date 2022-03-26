9242160d46f2bbe629a4f100c79235ffc608c89d
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-26 22:34:54 +0530

    added binary program

--- src/Day6/LogicalPrograms.java
+++ src/Day6/LogicalPrograms.java
@@ -1,39 +1,22 @@
 package Day6;
 public class LogicalPrograms {
-	public static int main(String[] args) {
-	int i;
-	int total;
-	
-   int[] notes = { 1000,500,100,50,10,5,2,1};
-   int money;
-  	
-  	
-        int rem;
-		if(money==0)
-		{
-			return -1 ;
-		}
-		else
-		{
-			if(money>=notes[i])
-			{
-				int calNotes =money/notes[i];
-				rem = money%notes[i];
-				money =rem;
-				total += calNotes;
-				System.out.println(notes[i]+   " Notes ---> " +calNotes );
-			}
-			i++;
-			return calculate(money, notes);
-		}
+	static int swapNibbles(int x)
+	{
+	    return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
 	}
+	 
+	// Driver code
+	public static void main(String arg[])
+	{
+	    int x = 100;
+	    System.out.print(swapNibbles(x));
+	}
+}
 
-	private static int calculate(int money, int[] notes) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+
 	
-}
-}
+
+
+
 
 
