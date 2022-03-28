711a7ce47cbc5622401653c9a50dd905bbea11a8
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-28 10:03:20 +0530

    added monthly payment program

--- src/Day6/LogicalPrograms.java
+++ src/Day6/LogicalPrograms.java
@@ -4,23 +4,13 @@ import java.util.Scanner;
 
 public class LogicalPrograms {
 	public static void main(String[] args) {
-		try {
-			Scanner s = new Scanner(System.in);
-			System.out.println("enter temp in c or f");
-			int tem = s.nextInt();
-			char t = s.next().charAt(0);
-			s.close();
-			if (t != 'f' && t != 'F' && t != 'c' && t != 'C') {
-				System.out.println("enter correct input");
-				main(args);
-				return;
-			}
-			tem = Util.temperaturConversion(tem, t);
-			System.out.println("coverted temp is " + tem);
-		} catch (Exception e) {
-			System.out.println("temperature in format 4545 c");
-		}
-
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter principal ,year and rate");
+		double p = s.nextDouble();
+		double y = s.nextDouble();
+		double r = s.nextDouble();
+		s.close();
+		System.out.println("mothly payment is " + Util.logicalprograms(p, y, r));
 	}
 }
 
