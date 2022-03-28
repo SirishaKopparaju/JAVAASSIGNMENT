928546b9b04d9b19c5691349dc784e7545d06748
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-28 10:11:52 +0530

    refactor square root program

--- src/Day6/LogicalPrograms.java
+++ src/Day6/LogicalPrograms.java
@@ -3,14 +3,17 @@ package Day6;
 import java.util.Scanner;
 
 public class LogicalPrograms {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("enter principal ,year and rate");
-		double p = s.nextDouble();
-		double y = s.nextDouble();
-		double r = s.nextDouble();
-		s.close();
-		System.out.println("mothly payment is " + Util.logicalprograms(p, y, r));
+	public static void main(String[] args) throws Exception {
+		try {
+			Scanner s = new Scanner(System.in);
+			System.out.println("enter no to find square root");
+			double c = s.nextDouble();
+			System.out.println("square root is " + Util.sqrt(c));
+			s.close();
+		} catch (Exception e) {
+			System.out.println("inter integer value "+e.getMessage());
+		}
+
 	}
 }
 
